98da0d0f1a1f5c6712e682a7aefeeb643b67bb69
Halleluyah Adebisi <49336838+harlesky@users.noreply.github.com>
2025-03-10 14:05:16 +0100

    Update Project_porfolio_from_Alex_Analyst.sql

--- SQL_tutorial_&_project/Project_porfolio_from_Alex_Analyst.sql
+++ SQL_tutorial_&_project/Project_porfolio_from_Alex_Analyst.sql
@@ -137,4 +137,29 @@ FROM layoffs_staging2
 WHERE total_laid_off IS NULL
 AND percentage_laid_off IS NULL;
 
-SELECT 
+-- 4. remove any columns and rows we need to
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL;
+
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+-- Delete Useless data we can't use
+DELETE FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+SELECT * 
+FROM world_layoffs.layoffs_staging2;
